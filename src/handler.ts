07019eaaa20310141d4cc29df7b43064e99ecaae
Jaeseung Lee(Home) <41176085+tkxkd0159@users.noreply.github.com>
2022-02-16 04:31:30 +0900

    feat: Make basic interactive chat

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,22 @@
 import WebSocket from "ws";
 
-function handleConn(cliSock: WebSocket){
+// interface SendList {
+//     [nickname: string]: WebSocket
+// }
+
+interface chatWebSocket extends WebSocket {
+    nickname: string
+}
+
+const clients: chatWebSocket[] = [];
+
+function parseMsg(msg: string) {
+    return JSON.parse(msg);
+}
+
+function handleConn(cliSock: chatWebSocket){
+    clients.push(cliSock);
+    cliSock["nickname"] = "";
     console.log("Connected to the client ✅")
 
     cliSock.on("close", () => {
@@ -8,10 +24,22 @@ function handleConn(cliSock: WebSocket){
     });
 
     cliSock.on("message", (msg: Buffer) => {
-        console.log(msg.toString())
-    })
+        const msgObj = parseMsg(msg.toString());
+        if (msgObj.type === "save_nickname") {
+            cliSock["nickname"] = msgObj.payload
+        }
+        else if (msgObj.type === "new_msg") {
+            const sender = cliSock.nickname;
+            for (let ws of clients) {
+                if (ws.nickname !== sender) {
+                    ws.send(`${sender}: ${msgObj.payload}`)
+                } else {
+                    ws.send(`> ${sender}: ${msgObj.payload}`)
+                }
+            }
+        }
 
-    cliSock.send("Hello!!!")
+    })
 }
 
 
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,7 @@
+const msgList = document.querySelector("ul#chat-history");
+const msgForm = document.querySelector("form.form-main-chat");
+const nickForm = document.querySelector("form.form-chat-nick");
+
 const ws = new WebSocket(`ws://${window.location.host}`)
 
 ws.addEventListener("open", () => {
@@ -6,6 +10,9 @@ ws.addEventListener("open", () => {
 
 ws.addEventListener("message", (msg) => {
     console.log(`From Server : ${msg.data}`, msg)
+    const newChatRow = document.createElement("li");
+    newChatRow.innerText = msg.data;
+    msgList.append(newChatRow);
 });
 
 ws.addEventListener("close", () => {
@@ -13,6 +20,27 @@ ws.addEventListener("close", () => {
 })
 
 
-setTimeout(() => {
-    ws.send("Hello from the browser after 10 secs")
-}, 1500);
+// setTimeout(() => {
+//     ws.send("Hello from the browser after 10 secs")
+// }, 1500);
+
+function makeMsg(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+function handleSubmit(e) {
+    e.preventDefault(); // submit 시 새로고침 방지
+    const input = msgForm.querySelector("input")
+    ws.send(makeMsg("new_msg", input.value));
+    input.value = "";
+}
+
+function handleNickSave(e) {
+    e.preventDefault(); // submit 시 새로고침 방지
+    const input = nickForm.querySelector("input")
+    ws.send(makeMsg("save_nickname", input.value));
+    input.value = "";
+}
+msgForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSave);
