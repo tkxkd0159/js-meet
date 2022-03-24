8283ee4068e85963ab4033cc09644819287327a0
Jaeseung Lee <41176085+tkxkd0159@users.noreply.github.com>
2022-03-24 23:49:10 +0900

    feat: Add welcome / change msg At legacy-ws

--- legacy-ws/src/handler.ts
+++ legacy-ws/src/handler.ts
@@ -26,7 +26,18 @@ function handleConn(cliSock: chatWebSocket){
     cliSock.on("message", (msg: Buffer) => {
         const msgObj = parseMsg(msg.toString());
         if (msgObj.type === "save_nickname") {
+            let oldName = cliSock["nickname"]
             cliSock["nickname"] = msgObj.payload
+            if (oldName !== "") {
+                for (let ws of clients) {
+                    ws.send(`${oldName} changed its name to ${cliSock["nickname"]}`)
+                }
+            } else {
+                for (let ws of clients) {
+                    ws.send(`${cliSock["nickname"]} enter the chat room`)
+                }
+            }
+
         }
         else if (msgObj.type === "new_msg") {
             const sender = cliSock.nickname;
