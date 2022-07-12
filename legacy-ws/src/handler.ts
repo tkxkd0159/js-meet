8d0aef3fa10b01a5e3690896571715bcfa545104
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2022-07-12 23:58:03 +0900

    chore: code formatting

--- legacy-ws/src/handler.ts
+++ legacy-ws/src/handler.ts
@@ -5,7 +5,7 @@ import WebSocket from "ws";
 // }
 
 interface chatWebSocket extends WebSocket {
-    nickname: string
+    nickname: string;
 }
 
 const clients: chatWebSocket[] = [];
@@ -14,46 +14,42 @@ function parseMsg(msg: string) {
     return JSON.parse(msg);
 }
 
-function handleConn(cliSock: chatWebSocket){
+function handleConn(cliSock: chatWebSocket) {
     clients.push(cliSock);
     cliSock["nickname"] = "";
-    console.log("Connected to the client ✅")
+    console.log("Connected to the client ✅");
 
     cliSock.on("close", () => {
-        console.log("Disconnected from the client ❌")
+        console.log("Disconnected from the client ❌");
     });
 
     cliSock.on("message", (msg: Buffer) => {
         const msgObj = parseMsg(msg.toString());
         if (msgObj.type === "save_nickname") {
-            let oldName = cliSock["nickname"]
-            cliSock["nickname"] = msgObj.payload
+            let oldName = cliSock["nickname"];
+            cliSock["nickname"] = msgObj.payload;
             if (oldName !== "") {
                 for (let ws of clients) {
-                    ws.send(`${oldName} changed its name to ${cliSock["nickname"]}`)
+                    ws.send(
+                        `${oldName} changed its name to ${cliSock["nickname"]}`
+                    );
                 }
             } else {
                 for (let ws of clients) {
-                    ws.send(`${cliSock["nickname"]} enter the chat room`)
+                    ws.send(`${cliSock["nickname"]} enter the chat room`);
                 }
             }
-
-        }
-        else if (msgObj.type === "new_msg") {
+        } else if (msgObj.type === "new_msg") {
             const sender = cliSock.nickname;
             for (let ws of clients) {
                 if (ws.nickname !== sender) {
-                    ws.send(`${sender}: ${msgObj.payload}`)
+                    ws.send(`${sender}: ${msgObj.payload}`);
                 } else {
-                    ws.send(`> ${sender}: ${msgObj.payload}`)
+                    ws.send(`> ${sender}: ${msgObj.payload}`);
                 }
             }
         }
-
-    })
+    });
 }
 
-
-export {
-    handleConn
-}
+export { handleConn };
--- legacy-ws/src/server.ts
+++ legacy-ws/src/server.ts
@@ -4,7 +4,7 @@ import express from "express";
 
 import { handleConn } from "./handler";
 
-const MODE = "dev"
+const MODE = "dev";
 const PORT = 3000;
 const app = express();
 app.set("view engine", "pug");
@@ -17,9 +17,11 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
 
-const handleListen = () => {console.log(`Listening on http://localhost:${PORT}`)}
+const handleListen = () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+};
 const server = http.createServer(app);
-server.listen(PORT, handleListen)
+server.listen(PORT, handleListen);
 
 const wss = new WebSocket.Server({ server });
 wss.on("connection", handleConn);
